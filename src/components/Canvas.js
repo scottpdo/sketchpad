bc4ac1d16e71d3941f4d382bbd4892d28209df68
Scott Donaldson <scott.p.donaldson@gmail.com>
2017-06-19 14:17:28 -0400

    draw lines, circles

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,6 +1,52 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 
+class Line {
+  constructor(x1, y1, x2, y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  update(e) {
+    this.x2 = e.x;
+    this.y2 = e.y;
+  }
+
+  draw(context) {
+    context.strokeStyle = 'rgb(255, 255, 255)';
+    context.lineWidth = 2;
+    context.beginPath();
+    context.moveTo(this.x1, this.y1);
+    context.lineTo(this.x2, this.y2);
+    context.stroke();
+  }
+}
+
+class Circle {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+
+  update(e) {
+    const dx = e.x - this.x;
+    const dy = e.y - this.y;
+    const r = Math.sqrt(dx * dx + dy * dy);
+    this.r = r;
+  }
+
+  draw(context) {
+    context.strokeStyle = 'rgb(255, 255, 255)';
+    context.lineWidth = 2;
+    context.beginPath();
+    context.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    context.stroke();
+  }
+}
+
 class Canvas extends Component {
 
   constructor() {
@@ -11,10 +57,45 @@ class Canvas extends Component {
     const refreshRate = 10;
 
     this.state = {
-      t: 0
+      t: 0,
+      drawing: false,
+      cursor: { x: 0, y: 0 }
     };
 
+    this.keys = {
+      // CIRCLE
+      67: e => {
+        this.setState({ drawing: !this.state.drawing }, () => {
+
+          if (!this.state.drawing) return this.activeObj = null; // finished drawing
+
+          let circle = new Circle(e.x, e.y, 0);
+          
+          // set as active object and push to objects
+          this.activeObj = circle;
+          this.objects.push(circle);
+        });
+      },
+      // LINE
+      76: e => {
+        this.setState({ drawing: !this.state.drawing }, () => {
+
+          if (!this.state.drawing) return this.activeObj = null; // finished drawing
+
+          let line = new Line(e.x, e.y, e.x, e.y);
+          
+          // set as active object and push to objects
+          this.activeObj = line;
+          this.objects.push(line);
+        });
+      }
+    };
+
+    this.activeObj = null;
+    this.objects = [];
+
     this.onResize = this.onResize.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
 
     this.update = _.throttle(
       this.update.bind(this),
@@ -28,6 +109,10 @@ class Canvas extends Component {
     canvas.height = window.innerHeight;
   }
 
+  onKeyUp(e) {
+    if (this.keys[e.keyCode]) this.keys[e.keyCode](this.state.cursor);
+  }
+
   update(e) {
 
     let t = this.state.t;
@@ -35,18 +120,50 @@ class Canvas extends Component {
     const canvas = this.refs.canvas;
     const context = canvas.getContext('2d');
 
-    const randColor = () => Math.round((Math.random() * 255));
-    const randRGB = () => 'rgb(' + randColor() + ',' + randColor() + ',' + randColor() + ')';
-
     context.fillStyle = 'rgb(0, 0, 0)';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     if (e) {
-      context.fillStyle = 'rgb(255, 255, 255)';
+
+      this.setState({
+        cursor: { x: e.x, y: e.y }
+      });
+
+      context.strokeStyle = 'rgb(255, 255, 255)';
+      context.lineWidth = 2;
+
       context.beginPath();
-      context.arc(e.x, e.y, 50, 0, 2 * Math.PI);
+
+      // left
+      context.moveTo(e.x - 20, e.y);
+      context.lineTo(e.x - 5, e.y);
+      // right
+      context.moveTo(e.x + 5, e.y);
+      context.lineTo(e.x + 20, e.y);
+      // top
+      context.moveTo(e.x, e.y + 5);
+      context.lineTo(e.x, e.y + 20);
+      // bottom
+      context.moveTo(e.x, e.y - 5);
+      context.lineTo(e.x, e.y - 20);
+
+      context.stroke();
+
+      context.lineWidth = 0;
+      context.fillStyle = 'rgb(255, 255, 255)';
+
+      context.beginPath();
+      context.arc(e.x, e.y, 2, 0, 2 * Math.PI);
       context.fill();
     }
+
+    // update active object
+    if (this.activeObj) this.activeObj.update(e);
+
+    // draw objects
+    this.objects.forEach((obj) => {
+      obj.draw(context);
+    });
 
     t++;
     this.setState({ t });
@@ -63,6 +180,7 @@ class Canvas extends Component {
 
     window.addEventListener('resize', this.onResize);
     window.addEventListener('mousemove', this.update);
+    window.addEventListener('keyup', this.onKeyUp);
   }
 
   render() {
